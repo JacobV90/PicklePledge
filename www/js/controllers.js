de92f032a95b53064880e13eccafdaf40c3e0549
Jacob Veal <jacob-veal@uiowa.edu>
2017-04-15 15:56:37 -0500

    done with user log. need to implement side menu

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -34,8 +34,8 @@ function ($scope, $stateParams, Auth, $ionicLoading, $ionicPopup, $state) {
 
 }])
 
-.controller('signupCtrl', ['$scope', '$state', 'Auth', '$ionicLoading', '$firebaseArray',
-function ($scope, $state, Auth, $ionicLoading, $firebaseArray) {
+.controller('signupCtrl', ['$scope', '$state', 'Auth', '$ionicLoading', '$firebaseArray','$rootScope',
+function ($scope, $state, Auth, $ionicLoading, $firebaseArray, $rootScope) {
 
   var users_ref = firebase.database().ref("Users");
   var users = $firebaseArray(users_ref);
@@ -49,18 +49,18 @@ function ($scope, $state, Auth, $ionicLoading, $firebaseArray) {
     Auth.$createUserWithEmailAndPassword(email, password)
         .then(function (newUser) {
 
-          users.$add({
-            email: email,
-            whine_fine: 0
-          })
-
           $ionicLoading.hide()
 
-          $state.go('chooseOrganization', {
-            email: email
-          },{
-            reload: true
-          })
+            users.$add({
+                email: email,
+                org: ""
+            }).then(function(){
+
+                $state.go('chooseOrganization', {
+                    reload: true
+                })
+            });
+
     }).catch(function (error) {
           $ionicLoading.hide()
           alert(error)
@@ -101,7 +101,7 @@ function ($scope, $state, Auth, $ionicLoading, $firebaseArray) {
             $state.go('chooseOrganization', {
               email: email
             },{
-              reload: true
+              reload: false
             })
 
       }).catch(function (error) {
@@ -116,13 +116,31 @@ function ($scope, $state, Auth, $ionicLoading, $firebaseArray) {
   }
 })
 
-.controller('chooseOrganizationCtrl', ['$scope', '$stateParams', "$firebaseArray", "$state", "SearchFilter",
-function ($scope, $stateParams, $firebaseArray, $state, SearchFilter) {
+.controller('chooseOrganizationCtrl', ['$scope', "$firebaseArray", "$state", "SearchFilter",'Auth','$firebaseObject', "$ionicLoading",
+function ($scope, $firebaseArray, $state, SearchFilter, Auth, $firebaseObject, $ionicLoading) {
 
-  var orgs_ref = firebase.database().ref("Orgs");
-  var orgs = $firebaseArray(orgs_ref);
-  var users_ref = firebase.database().ref("Users");
-  var selectedOrg;
+    var orgs_ref = firebase.database().ref("Orgs");
+    var orgs = $firebaseArray(orgs_ref);
+    var date = new Date().toDateString();
+
+
+    var user_auth = Auth.$getAuth();
+    var users_ref = firebase.database().ref("Users/");
+    var users = $firebaseArray(users_ref);
+    var current_user = null;
+    var selectedOrg;
+
+    users.$loaded().then(function(){
+        angular.forEach(users, function(user){
+            if(user.email == user_auth.email){
+                console.log(user.$id)
+                var current_user_ref = firebase.database().ref("Users/"+user.$id+"/");
+                var log_ref = firebase.database().ref("Users/"+user.$id+"/"+date);
+                current_user = $firebaseObject(current_user_ref);
+                current_log = $firebaseObject(log_ref);
+            }
+        })
+    })
 
   $scope.orgs = [];
 
@@ -154,14 +172,17 @@ function ($scope, $stateParams, $firebaseArray, $state, SearchFilter) {
 
   $scope.submitOrg = function(){
     if(selectedOrg){
-      var users = $firebaseArray(users_ref);
-      users.$add({
-        email: $stateParams.email,
-        org: selectedOrg,
-        whine_fine: 0
-      }).then(function(){
+
+      current_user.org = selectedOrg;
+      current_user.$save();
+
+      current_log.whine_fine = 0;
+      current_log.$save();
+
+      console.log(current_user)
+
         $state.go('pickleJar')
-      })
+
     }else{
       console.log("org not selected")
     }
@@ -178,28 +199,47 @@ function ($scope, $stateParams, $firebaseArray, $state, SearchFilter) {
 
 }])
 
-.controller('pickleJarCtrl', ['$scope', '$stateParams', '$firebaseObject', '$state', 'Auth',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('pickleJarCtrl', ['$scope', '$stateParams', '$firebaseArray', '$state', 'Auth','$firebaseObject', "$ionicSideMenuDelegate",// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams, $firebaseObject, $state, Auth) {
+function ($scope, $stateParams, $firebaseArray, $state, Auth, $firebaseObject,  $ionicSideMenuDelegate) {
 
   var user_auth = Auth.$getAuth();
-  var users_ref = firebase.database().ref("Users/"+user_auth.uid);
+  var users_ref = firebase.database().ref("Users/");
+  var users = $firebaseArray(users_ref);
+  var current_user;
+  $scope.total = 0;
+  $scope.date = new Date().toDateString();
+  var current_log = null;
+
+
+  users.$loaded().then(function(){
+    angular.forEach(users, function(user){
+      if(user.email == user_auth.email){
+          var log_ref = firebase.database().ref("Users/"+user.$id+"/"+$scope.date);
+          current_log = $firebaseObject(log_ref);
+          console.log(user.$id)
+      }
+    });
+
+    $scope.total = current_log.whine_fine;
+
+  });
+
+    $scope.toggleLeftSideMenu = function() {
+        $ionicSideMenuDelegate.toggleLeft();
+    };
 
   $scope.logout = function(){
     firebase.auth().signOut().then(function(){
       $state.go("login", {reload: true})
     })
-  }
+  };
 
   $scope.addWhineFine = function(){
-    var user = $firebaseObject(users_ref);
-    console.log(user_auth)
-    user.whine_fine += 0.25
-    user.$save().then(function(){
-      console.log("successfully added money")
-    })
-
+      current_log.whine_fine += 0.25;
+      current_log.$save(current_user);
+      $scope.total = current_log.whine_fine;
   }
 
 
